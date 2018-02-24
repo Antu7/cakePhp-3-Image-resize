758d110bc4587c1ce55387adeb072994c40bd908
Antu7 <tanvir.antu7@gmail.com>
2018-02-24 15:56:15 +0600

    Image Resize complt

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -25,77 +25,89 @@ class PostController extends AppController
 
         $post = "";
 
+
         if ($this->request->is('post')) {
             if (!empty($this->request->data['image']['name'])) {
 
 
-                $image_hight = $this->request->data['image_hight[]'];
-                $image_weight = $this->request->data['image_weight[]'];
+                $image_hight = $this->request->data['image_hight'];
+                $image_weight = $this->request->data['image_weight'];
 
+                $count = $image_hight;
 
-echo "<pre>";
-
-                print_r($image_hight);
-                print_r($image_weight);
-
-                die();
 
                 $filename = $this->request->data['image']['tmp_name'];
-                //   $try = $this->request->data['image']['tmp_name'];
+
 
                 $ext = ".jpg";  ///get ext
 
                 list($width, $height) = getimagesize($filename);
-//                var_dump($name);
-//
-//                die();
 
                 $src = imagecreatefromjpeg($filename);
 
-//                var_dump($src);
-//                die();
-
-                $newheight = $image_hight;
-                $newwidth = $image_weight;                   /// variable()
-                /// $newheight = $newhight;      /// ($height / $width) * $newwidth;
-                $tmp = imagecreatetruecolor($newwidth, $newheight);
-
-
-                imagecopyresampled($tmp, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+                for ($i = 0; $i < count($count); $i++) {
+//                    $my_count = $i;
+                    // echo  $my_count;
+                    $newheight = $image_hight[$i];
+                    $newwidth = $image_weight[$i];                   /// variable()
+                    /// $newheight = $newhight;      /// ($height / $width) * $newwidth;
+                    $tmp = imagecreatetruecolor($newwidth, $newheight);
 
 
-                var_dump($src);
+                    imagecopyresampled($tmp, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
 
 
-//                $new_image =   $filename;
+                    imagejpeg($tmp, $filename, 100);
+
+
+                    $rnd_name = 'photos_' . uniqid(mt_rand(10, 15)) . '_' . time() . '_50x50.' . $ext;
+
+
+                    $url = Router::url('/', true) . 'image/' . $rnd_name;
+                    $uploadpath = 'image/';
+                    $uploadfile = $uploadpath . $rnd_name;
+                    var_dump($filename);
+                    echo "<br>";
+
+                    $filename = $this->request->data['image']['tmp_name'];
+
+
+//                    if ($my_count == 1) {
+//                        $filename = $filename . 'name';
+//                        var_dump($filename);
+//                        echo "<br>";
+//                        if (move_uploaded_file($filename, $uploadfile)) {
 //
-//                var_dump($new_image);
+//                            $post = $this->Posts->newEntity();
+//                            $post->name = $filename;
+//                            $post->image = $url;
+//                            if ($this->Posts->save($post)) {
+//                                $this->Flash->success(__('File Upload first'));
 //
-//
-                imagejpeg($tmp, $filename, 100);
-//                var_dump($try);
+//                            } else {
+//                                $this->Flash->error(__('File Upload Fail              1'));
+//                            }
+//                        } else {
+//                            $this->Flash->error(__('File Upload Fail                     2 first'));
+//                        }
+//                    } else {}
 
-                $rnd_name = 'photos_' . uniqid(mt_rand(10, 15)) . '_' . time() . '_50x50.' . $ext;
 
+                    if (move_uploaded_file($filename, $uploadfile)) {
 
-                $url = Router::url('/', true) . 'image/' . $rnd_name;
-                $uploadpath = 'image/';
-                $uploadfile = $uploadpath . $rnd_name;
+                        $post = $this->Posts->newEntity();
+                        $post->name = $filename;
+                        $post->image = $url;
+                        if ($this->Posts->save($post)) {
+                            $this->Flash->success(__('File Upload second'));
 
-//                $this->request->data['image']['tmp_name']
-                if (move_uploaded_file($filename, $uploadfile)) {
-
-                    $post = $this->Posts->newEntity();
-                    $post->name = $filename;
-                    $post->image = $url;
-                    if ($this->Posts->save($post)) {
-                        $this->Flash->success(__('File Upload'));
-
+                        } else {
+                            $this->Flash->error(__('File Upload Fail      1'));
+                        }
                     } else {
-                        $this->Flash->error(__('File Upload Fail              1'));
+                        $this->Flash->error(__('File Upload Fail    2 second'));
+
                     }
-                } else {
-                    $this->Flash->error(__('File Upload Fail                     2'));
                 }
             } else {
                 $this->Flash->error(__('choose file'));
